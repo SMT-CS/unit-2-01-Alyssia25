0d9f5933c6b9d753d5f82327a536b467a7bd514d
Alyssia25 <alyssia.fung@stu.ocsb.ca>
2025-02-25 19:35:43 +0000

    added circumference

---
+++ circumference.c
@@ -0,0 +1,18 @@
+// Copyright (c) 2025 Alyssia Fung All rights reserved
+//
+// Created by: Alyssia Fung
+// Created on: feb 2025
+// This program calculates the circumference of a circle
+// with a diameter of 5 mm.
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // this function calculates the circumference of a 42 mm diameter circle
+    printf("If a circle has a diameter of 5 mm: \n");
+    printf("\n");
+    printf("Circumference is %.2f mm.\n", (M_PI * 5));
+    printf("\nDone.\n");
+#include <cstdio> for printf return 0;
+}
---
+++ circumference.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Created by: Alyssia Fung
+Created on: Feb 2025
+This module calculates the circumference of a circle with diamiter 5 mm.
+"""
+
+import math
+
+
+def main() -> None:
+    """
+    The main() function calculates the area of a 5 mm circle,
+        returns None.
+    """
+    print("If a circle has a radius of 5 mm:")
+    print("")
+    print(f"circumference is {(math.pi * 5):.2f} mm.")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
